9348322b584b535a2b61c8a0ad32ac113bd67e81
Xiaojun Feng <junjun80585@gmail.com>
2011-12-23 20:08:00 -0500

    code

--- src/sam_model.py
+++ src/sam_model.py
@@ -5,8 +5,9 @@ import numpy as np
 import random
 from scipy import integrate
 ALPHA = 1.0
-NUM = 7
-T=1000
+NUM = 10
+T=10000
+E_LIMIT = -0.0001
 def calculate_feature_score(feature_idx,sent_dict):
     #position is the key, value is the sentiment word in the position
     pos_sent_dict = dict([(idx,w) for w in sent_dict.keys()
@@ -58,7 +59,7 @@ def sam_parameters(f_s_dict):
     feature_total = reduce(lambda x,y:x+y,map(lambda x:len(x),f_s_dict.values()))
     return  dict([(f,{'prob':float(len(v))/feature_total,
                          'mu':np.average(np.array(v)),
-                         'sigma':np.var(np.array(v))})
+                         'sigma':np.var(np.array(v))+0.01})
                         for f,v in f_s_dict.items()])
 
 
@@ -72,7 +73,7 @@ def merge_feature_score(f_s_list):
 
 def KL_divergence(total_param, summary_param):
     valid_param = filter(lambda f:
-            summary_param[f]['sigma']>0.01,summary_param.keys())
+            total_param[f]['sigma']>0.01,total_param.keys())
     def KL_discrete(p1,p2):
         return p1*math.log(p1/p2)+(1-p1)*math.log((1-p1)/(1-p2))
 
@@ -91,15 +92,26 @@ def KL_divergence(total_param, summary_param):
             +KL_norm(total_param[f]['mu'],summary_param[f]['mu'],
                      total_param[f]['sigma'],summary_param[f]['sigma'])
                     for f in valid_param]
+    if not kl:
+        return -100
     return -sum(kl)
 
-
+def extend_param(summary_param,total_param):
+    param_add = set(total_param.keys()).difference(set(summary_param.keys()))
+    def add2dict(f):
+        summary_param[f]={'prob':0.0001,
+                          'mu':0,
+                          'sigma':0.01}
+    map(add2dict,param_add)
+    return summary_param
 
 def get_summary(feature_file,sentiment_file,sentence_file,output_file):
 
     cin = open(sentence_file)
     sentences = cin.readlines()
     cin.close()
+
+    cout = open(output_file,'w')
 
     f_s_list,intensity_list = preprocess_file(feature_file,sentiment_file)
 
@@ -113,7 +125,7 @@ def get_summary(feature_file,sentiment_file,sentence_file,output_file):
 
     # remove useless features
     valid_features = [f for f,v in total_param.items()
-                        if v['sigma']!=0 or v['mu']!=0]
+                        if v['sigma']>0.01 or v['mu']!=0]
     f_s_list = [dict([(f,v) for f,v in f_dict.items()
                             if f in valid_features])
                             for f_dict in f_s_list]
@@ -125,61 +137,66 @@ def get_summary(feature_file,sentiment_file,sentence_file,output_file):
     total_param = sam_parameters(merge_dict)
 
 
+    for rr in range(1):
     #initial summary
-    sent_list = range(len(f_s_list))
-    random.shuffle(sent_list)
-    old_summary = [f_s_list[i] for i in sent_list[:NUM]]
-    merge_dict = merge_feature_score(old_summary)
-    summary_param = sam_parameters(merge_dict)
-
-    oldE = KL_divergence(total_param,summary_param)
-
-    def swap(i,j):
-        temp=sent_list[i]
-        sent_list[i] = sent_list[j]
-        sent_list[j] = temp
-
-    P=0.8
-    for t in range(T):
-#        print sent_list[:5]
-        i = random.randrange(0,NUM)
-        j = random.randrange(NUM,len(sent_list))
-
-        swap(i,j)
-
-        new_summary = [f_s_list[i] for i in sent_list[:NUM]]
-        merge_dict = merge_feature_score(new_summary)
+        sent_list = range(len(f_s_list))
+        random.shuffle(sent_list)
+        old_summary = [f_s_list[i] for i in sent_list[:NUM]]
+        merge_dict = merge_feature_score(old_summary)
         summary_param = sam_parameters(merge_dict)
+        summary_param = extend_param(summary_param,total_param)
 
-        E = KL_divergence(total_param,summary_param)
+        oldE = KL_divergence(total_param,summary_param)
 
-        diffE = E-oldE
-        if diffE >= 0:
-#            print "swap"
-            oldE = E
-#            old_summary = new_summary
-        else:
-            r = random.random()
-            if r > P:
-                swap(i,j)
-            else:
-#                print "swap"
+        def swap(i,j):
+            temp=sent_list[i]
+            sent_list[i] = sent_list[j]
+            sent_list[j] = temp
+
+        P=0.8
+        for t in range(T):
+            if t%500==0:
+                print t
+            
+    #        print sent_list[:5]
+            i = random.randrange(0,NUM)
+            j = random.randrange(NUM,len(sent_list))
+
+            swap(i,j)
+
+            new_summary = [f_s_list[i] for i in sent_list[:NUM]]
+            merge_dict = merge_feature_score(new_summary)
+            summary_param = sam_parameters(merge_dict)
+            summary_param = extend_param(summary_param,total_param)
+
+            E = KL_divergence(total_param,summary_param)
+
+            diffE = E-oldE
+            if diffE >= 0:
+    #            print "swap"
                 oldE = E
-#                old_summary = new_summary
-                
-        P *=0.99
+    #            old_summary = new_summary
+            else:
+                r = random.random()
+                if r > P:
+                    swap(i,j)
+                else:
+    #                print "swap"
+                    oldE = E
+    #                old_summary = new_summary
 
-    cout = open(output_file,'w')
-    for i in range(NUM):
-        cout.write(sentences[sent_list[i]])
+            P *=0.99
+            if oldE>E_LIMIT:
+                break
+
+
+        cout.write('summary %d -- score %f\n' %(rr,oldE))
+        for i in range(NUM):
+            cout.write(sentences[sent_list[i]])
+        cout.write('\n')
     cout.close()
 
     return valid_features
-
-
-
-
-
 
 
 #    sent_param = map(calculate_parameters,f_s_list)
@@ -195,21 +212,22 @@ if __name__=='__main__':
 
     feature_files = sorted([os.path.join(feature_dir,f) for f in os.listdir(feature_dir)
                             if f.endswith('.txt')])
-
+    print feature_files
     sentiment_files = sorted([os.path.join(sentiment_dir,f) for f in os.listdir(sentiment_dir)
                             if f.endswith('.txt')])
     sentence_files = sorted([os.path.join(sentence_dir,f) for f in os.listdir(sentence_dir)
                             if f.endswith('.txt')])
 
-    for i in range(len(feature_files)):
-        output_file = os.path.join(summary_dir,feature_files[i].split('/')[-1].split('_')[0]+'.summary')
-        valid_feature = os.path.join(valid_feature_dir,feature_files[i].split('/')[-1].split('_')[0]+'.features')
+#    for i in range(len(feature_files)):
+    i=2
+    output_file = os.path.join(summary_dir,feature_files[i].split('/')[-1].split('_')[0]+'.summary')
+    valid_feature = os.path.join(valid_feature_dir,feature_files[i].split('/')[-1].split('_')[0]+'.features')
 
-        features = get_summary(feature_files[i],sentiment_files[i],sentence_files[i],output_file)
+    features = get_summary(feature_files[i],sentiment_files[i],sentence_files[i],output_file)
 
-        cout = open(valid_feature,'w')
-        cout.write(json.dumps(features))
-        cout.close()
+    cout = open(valid_feature,'w')
+    cout.write(json.dumps(features))
+    cout.close()
 
 #    print feature_files
 #    print sentiment_files
